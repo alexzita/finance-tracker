148fb3bbd3b63854c2ddcc1dbfe9f9875c9002e3
alexzita <alexzita5@gmail.com>
2017-09-30 21:34:22 -0400

    finalization of finance-tracker

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -18,6 +18,26 @@ $('#stock-lookup-results').replaceWith('');
 $('#stock-lookup-errors').replaceWith('Stock was not found.');
 
 });
+$('#stock-lookup-form').on('ajax:before', function(event, data, status){
+
+show_spinner();
+
+});
+
+$('#stock-lookup-form').on('ajax:after', function(event, data, status){
+
+hide_spinner();
+
+});
+$('#stock-lookup-form').on('ajax:error', function(event, xhr, status, error){
+
+hide_spinner();
+
+$('#stock-lookup-results').replaceWith(' ');
+
+$('#stock-lookup-errors').replaceWith('Stock was not found.');
+
+});
 }
 
 $(document).ready(function() {
